0ed3120bcd735e195824b8ed6b015c697420ec97
chengang <769063757@qq.com>
2017-08-17 12:12:35 +0800

    总结

--- js总结.js
+++ js总结.js
@@ -103,3 +103,30 @@ getX(); // 9, 因为在这个例子中，"this"指向全局对象
 // 创建一个'this'绑定到module的函数
 var boundGetX = getX.bind(modu);
 console.log(boundGetX()); // 81
+
+
+/*
+*
+*1）创建新节点
+ createDocumentFragment() //创建一个DOM片段
+ createElement() //创建一个具体的元素
+ createTextNode() //创建一个文本节点
+ 2）添加、移除、替换、插入
+ appendChild() //添加
+ removeChild() //移除
+ replaceChild() //替换
+ insertBefore() //插入
+ 3）查找
+ getElementsByTagName() //通过标签名称
+ getElementsByName() //通过元素的Name属性的值
+ getElementById() //通过元素Id，唯一性
+*
+* */
+
+
+
+
+
+
+
+
